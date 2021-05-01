8239a0ff62eb13983ea9413d7d5f218e6aa977ad
Igor Valet <igor_valet@hotmail.com>
2021-05-01 18:27:23 +0200

    [sensor] Better test script

--- sensor.cs
+++ sensor.cs
@@ -3,39 +3,48 @@ public Program() {
 }
 
 void Main(string argument) {
+    var args = argument.Split(' ').ToList();
+    string name;
+    if (args.Count < 1 && args[0].Length == 0) {
+        return;
+    }
+    name = args[0];
+
     var sensors = new List<IMySensorBlock>();
     GridTerminalSystem.GetBlocksOfType<IMySensorBlock>(sensors);
     foreach(var sensor in sensors) {
-        Echo("Sensor '" + sensor.Name + "' " + sensor.EntityId);
+        if(sensor.CustomName == name) {
+            Echo("Sensor '" + sensor.Name + "' " + sensor.EntityId);
 
-        sensor.DetectPlayers = false;
-        sensor.DetectFloatingObjects = false;
-        sensor.DetectAsteroids = false;
-        sensor.DetectEnemy = false;
+            sensor.DetectPlayers = false;
+            sensor.DetectFloatingObjects = false;
+            sensor.DetectAsteroids = false;
 
-        sensor.DetectSubgrids = true;
-        sensor.DetectSmallShips = true;
-        sensor.DetectLargeShips = true;
-        sensor.DetectStations = true;
-        sensor.DetectOwner = true;
-        sensor.DetectFriendly = true;
-        sensor.DetectNeutral = true;
+            sensor.DetectSubgrids = true;
+            sensor.DetectSmallShips = true;
+            sensor.DetectLargeShips = true;
+            sensor.DetectStations = true;
+            sensor.DetectOwner = true;
+            sensor.DetectFriendly = true;
+            sensor.DetectNeutral = true;
 
-        sensor.LeftExtend = 0.0f;
-        sensor.RightExtend = 0.0f;
-        sensor.TopExtend = 0.0f;
-        sensor.BottomExtend = 0.0f;
-        sensor.FrontExtend = 10.0f;
-        sensor.BackExtend = 0.0f;
+            sensor.LeftExtend = 0.0f;
+            sensor.RightExtend = 0.0f;
+            sensor.TopExtend = 0.0f;
+            sensor.BottomExtend = 0.0f;
+            sensor.FrontExtend = 50.0f;
+            sensor.BackExtend = 0.0f;
+            sensor.DetectEnemy = true;
 
-        var list = new List<MyDetectedEntityInfo>();
-        sensor.DetectedEntities(list);
-        foreach(var entity in list) {
-            switch(entity.Type) {
-                case MyDetectedEntityType.SmallGrid:
-                case MyDetectedEntityType.LargeGrid:
-                    Echo("  Detected " + entity + " | " + entity.Type);
-                    break;
+            var list = new List<MyDetectedEntityInfo>();
+            sensor.DetectedEntities(list);
+            foreach(var entity in list) {
+                switch(entity.Type) {
+                    case MyDetectedEntityType.SmallGrid:
+                    case MyDetectedEntityType.LargeGrid:
+                        Echo("  Detected " + entity + " | " + entity.Type + " | " + entity.Position);
+                        break;
+                }
             }
         }
     }
